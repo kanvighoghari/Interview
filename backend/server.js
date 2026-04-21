d2c82871de00323e73c598abe74c4d001e0f06ac
Knv_19 <kanvighoghari@gmail.com>
2026-04-21 14:29:47 +0530

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 
 app.use(cors({
-    origin :"http://localhost:5173",
+    origin :"https://interview-frontend-1gcr.onrender.com",
     credentials:true
 }))
 app.use(express.json())
